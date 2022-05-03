259298cd6e755e1b0387852da0680bbfe9191e81
kimhyera <khr1828@naver.com>
2022-05-04 03:21:37 +0900

    New Deployment

---
+++ assets/page1.905e6b20.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as a,b as e}from"./index.563a0e2e.js";const s={},c=e("h1",null,"page1",-1),r=e("iframe",{width:"100%",height:"700",src:"https://www.kakaocorp.com/page/kakao/history"},null,-1),n=[c,r];function _(p,i,d,h,l,f){return t(),a("div",null,n)}var k=o(s,[["render",_]]);export{k as default};
